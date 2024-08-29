f5a1abb92e9df4ca8a5b13520a8f55fba2a845fa
eyakub175 <eyakub.bubt@gmail.com>
2024-08-29 22:52:56 +0600

    Create areaOfTriangle.js

---
+++ Js Basic/areaOfTriangle.js
@@ -0,0 +1,9 @@
+var x = 5;
+var y = 6;
+var z = 7;
+
+var semiParameter = (x+y+z)/2;
+
+var area = Math.sqrt(semiParameter*(semiParameter-x)*(semiParameter-y)*(semiParameter-z));
+
+console.log(area);
